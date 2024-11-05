facbdc81fcaedaccaf1852a41cafda3112ea5dcd
Eduard <shartner2006@mail.ru>
2024-11-05 10:59:39 +0500

    Barchar/start

---
+++ src/components/BarchartComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { BarChart } from '@/components/ui/chart-bar'
+const data = [
+  { name: 'January', total: 100 },
+  { name: 'February', total: 200 },
+  { name: 'March', total: 300 },
+  { name: 'April', total: 400 },
+  { name: 'May', total: 500 },
+  { name: 'June', total: 600 },
+  { name: 'July', total: 700 },
+  { name: 'August', total: 800 },
+  { name: 'September', total: 900 },
+  { name: 'October', total: 1000 },
+  { name: 'November', total: 1100 },
+  { name: 'December', total: 1200 },
+]
+
+</script>
+<template>
+  <div class="rounded-xl border border-slate-800 w-1/2 mt-5">
+    <BarChart class="dark:fill-white" :data="data" index="name" :categories="['total',]" :rounded-corners="4" :show-legend="true" :show-tooltip="true" :y-formatter="(tick, i) => {
+      return typeof tick === 'number'
+        ? `$ ${new Intl.NumberFormat('us').format(tick).toString()}`
+        : ''
+    }"/>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import Button from '@/components/ui/button/Button.vue'
 import { Calendar } from '@/components/ui/calendar'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import BarchartComponent from '@/components/BarchartComponent.vue'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import {
   DateFormatter,
   type DateValue,
@@ -16,26 +21,78 @@ const value = ref<DateValue>()
 
 <template>
   <div class="container">
-    <div class="p-5 flex border-b border-l border-r border-slate-200">
+    <div
+      class="p-5 flex flex-wrap border-b border-l border-r border-slate-800 dark:bg-slate-950 dark:text-white"
+    >
       <div class="flex items-center w-full justify-between">
         <h2 class="text-3xl font-bold tracking-tight">Dashboard</h2>
         <div class="flex">
-            <Popover>
-                <PopoverTrigger>
-                    <Button variant="outline" class="mr-2">
-                    <CalendarIcon class="mr-2 h-4 w-4" />
-                    <span>{{ value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date" }}</span>
-                </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    <Calendar  v-model="value" initial-focus/>
-                </PopoverContent>
-            </Popover>
+          <Popover>
+            <PopoverTrigger>
+              <Button variant="outline" class="mr-2">
+                <CalendarIcon class="mr-2 h-4 w-4" />
+                <span>{{
+                  value
+                    ? df.format(value.toDate(getLocalTimeZone()))
+                    : 'Pick a date'
+                }}</span>
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <Calendar v-model="value" initial-focus />
+            </PopoverContent>
+          </Popover>
 
           <Button>Download</Button>
         </div>
       </div>
+      <div class="mt-5 flex w-full justify-between">
+        <div class="dashboard-content">
+          <div class="card border rounded-xl border-slate-800">
+            <div class="card-content">
+              <h3 class="text-xl font-bold flex justify-between">Total Sales <span>$</span></h3>
+              <p class="text-lg font-light">$ 1,200</p>
+              <p class="text-sm">20.1% from last month</p>
+            </div>
+          </div>
+        </div>
+        <div class="dashboard-content">
+          <div class="card border rounded-xl border-slate-800">
+            <div class="card-content">
+              <h3 class="text-xl font-bold flex justify-between">Total Sales <span>$</span></h3>
+              <p class="text-lg font-light">$ 1,200</p>
+              <p class="text-sm">20.1% from last month</p>
+            </div>
+          </div>
+        </div>
+        <div class="dashboard-content">
+          <div class="card border rounded-xl border-slate-800">
+            <div class="card-content">
+              <h3 class="text-xl font-bold flex justify-between">Total Sales <span>$</span></h3>
+              <p class="text-lg font-light">$ 1,200</p>
+              <p class="text-sm">20.1% from last month</p>
+            </div>
+          </div>
+        </div>
+        <div class="dashboard-content">
+          <div class="card border rounded-xl border-slate-800">
+            <div class="card-content">
+              <h3 class="text-xl font-bold flex justify-between">Total Sales <span>$</span></h3>
+              <p class="text-lg font-light">$ 1,200</p>
+              <p class="text-sm">20.1% from last month</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <BarchartComponent/>
     </div>
   </div>
 </template>
-
+<style scoped>
+  .card{
+    padding: 20px;
+  }
+  .dashboard-content{
+    width: 23%;
+  }
+</style>
